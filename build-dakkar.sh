17824abe3c0e2efab4f30a3ab6b8e69f15453121
Florian Esser <android-dev@fesr.de>
2019-02-21 23:44:10 +0100

    Use my forks

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -122,9 +122,9 @@ function get_rom_type() {
                 extra_make_options="WITHOUT_CHECK_API=true"
                 ;;
             lineage160)
-                mainrepo="https://github.com/LineageOS/android.git"
-                mainbranch="lineage-16.0"
-                localManifestBranch="android-9.0"
+                mainrepo="https://github.com/fearlessiron/android.git"
+                mainbranch="lineage-16.0-treble"
+                localManifestBranch="android-9.0-nogapps"
                 treble_generate="lineage"
                 extra_make_options="WITHOUT_CHECK_API=true"
                 ;;
@@ -288,7 +288,7 @@ function clone_or_checkout() {
             git checkout origin/"$localManifestBranch"
         )
     else
-        git clone https://github.com/phhusson/"$repo" "$dir" -b "$localManifestBranch"
+        git clone https://github.com/fearlessiron/"$repo" "$dir" -b "$localManifestBranch"
     fi
 }
 
